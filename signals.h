65df54f362430ee436cee2dff8153d163c1aa167
odedsc <38476537+odedsc@users.noreply.github.com>
2018-04-18 16:38:28 +0300

    Update signals.h

--- signals.h
+++ signals.h
@@ -19,7 +19,7 @@ using namespace std;
 
 const string sig_list[] =
 {
-	"",				//0
+	"",			//0
 	"SIGHUP",		//1
 	"SIGINT",		//2
 	"SIGQUIT",		//3
@@ -35,7 +35,7 @@ const string sig_list[] =
 	"SIGPIPE",		//13
 	"SIGALRM",		//14
 	"SIGTERM",		//15
-	"SIGSTKFLT",	//16
+	"SIGSTKFLT",		//16
 	"SIGCHLD",		//17
 	"SIGCONT",		//18
 	"SIGSTOP",		//19
@@ -45,7 +45,7 @@ const string sig_list[] =
 	"SIGURG",		//23
 	"SIGXCPU",		//24
 	"SIGXFSZ",		//25
-	"SIGVTALRM",	//26
+	"SIGVTALRM",		//26
 	"SIGPROF",		//27
 	"SIGWINCH",		//28
 	"SIGIO",		//29
@@ -63,7 +63,7 @@ class Job
 		struct timeval time;
 		time_t susp_time;
 
-		Job(string name, int pid, bool susp):
+		job(string name, int pid, bool susp):
 			job_name(name),
 			pID(pid),
 			ID( Find_Min_Id()),
@@ -74,7 +74,7 @@ class Job
 };
 
 int smash_kill(pid_t pid, int sig);
-void smash_wait(vector<Job>::iterator pJob);
+void smash_wait(vector<job>::iterator pJob);
 
 
 #endif
