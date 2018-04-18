f14ad8d7cc4b17def50670497373099b03ad8b92
nitaivi <38476668+nitaivi@users.noreply.github.com>
2018-04-18 11:39:51 +0300

    Add files via upload

---
+++ signals.cpp
@@ -0,0 +1,25 @@
+/*
+ * signals.cpp
+ *
+ *  Created on: Apr 18, 2018
+ *      Author: os
+ */
+// contains signal handler funtions
+// contains the function/s that set the signal handlers
+
+/*******************************************/
+/* Name: handler_cntlc
+   Synopsis: handle the Control-C */
+#include "signals.h"
+
+int smash_kill(pid_t pid, int sig){
+	if (pid == 0 || pid == -1) return -1;
+	else{
+		cout << "signal " << sig_list[sig] << "was sent to pid " << pid << endl;
+		if (kill(pid,sig)==-1) return -1;
+	}
+	return 0;
+}
+
+
+
