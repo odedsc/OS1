b1c02eda3d402014c76aa03d1a4ce02b284b505f
nitaivi <38476668+nitaivi@users.noreply.github.com>
2018-04-19 20:36:41 +0300

    Update smash.cpp

--- smash.cpp
+++ smash.cpp
@@ -1,69 +1,84 @@
-/*	smash.cpp
+/*
+ * smash.cpp
+ *
+ *  Created on: Apr 19, 2018
+ *      Author: os
+ */
 main file. This file contains the main function of smash
 *******************************************************************/
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
-#include <iostream.h>
 #include "commands.h"
 #include "signals.h"
 #define MAX_LINE_SIZE 80
 #define MAXARGS 20
 
+#include "commands.h"
+#include "signals.h"
+
+using namespace std;
 
 char* L_Fg_Cmd;
-void* jobs = NULL; //This represents the list of jobs. Please change to a preferred type (e.g array of char*)
-char lineSize[MAX_LINE_SIZE]; 
+vector<Job> Jobs;
+char lineSize[MAX_LINE_SIZE];
 //**************************************************************************************
 // function name: main
 // Description: main function of smash. get command from user and calls command functions
 //**************************************************************************************
 int main(int argc, char *argv[])
 {
-	char cmdString[MAX_LINE_SIZE]; 	
-	
-	if (signal(SIGTSTP, smash_kill) != 0)
-	{
-		perror("signal error");
-	}
-    	if (signal(SIGINT,  smash_kill) != 0)
-	{
-		perror("signal error");
-	}
-    	if (signal(SIGCHLD, Smash_SIGCHLD) != 0)
-	{
-		perror("signal error");
-	}
-	
-	// Init globals 
+    char cmdString[MAX_LINE_SIZE];
+
+
+	//signal declaretions
+	//NOTE: the signal handlers and the function/s that sets the handler should be found in siganls.c
 	 /* add your code here */
-	
-	L_Fg_Cmd =(char*)malloc(sizeof(char)*(MAX_LINE_SIZE+1));
-	if (L_Fg_Cmd == NULL) 
-			exit (-1); 
-	L_Fg_Cmd[0] = '\0';
-	
+
+	/************************************/
+	//NOTE: the signal handlers and the function/s that sets the handler should be found in siganls.c
+	//set your signal handlers here
+	/* add your code here */
+    signal(SIGTSTP, aux_smash_kill);
+    signal(SIGINT,  aux_smash_kill);
+    signal(SIGCHLD, smash_SIGCHLD);
+	/************************************/
+
+	/************************************/
+	// Init globals
+
+    pid_t currFG_PID = -1;
+
+/*	L_Fg_Cmd =(char*)malloc(sizeof(char)*(MAX_LINE_SIZE+1));
+	if (L_Fg_Cmd == NULL)
+			exit (-1);
+	L_Fg_Cmd[0] = '\0';*/
+
     	while (1)
     	{
 	 	printf("smash > ");
 		fgets(lineSize, MAX_LINE_SIZE, stdin);
-		strcpy(cmdString, lineSize);    	
+		strcpy(cmdString, lineSize);
 		cmdString[strlen(lineSize)-1]='\0';
 					// perform a complicated Command
-		if(!ExeComp(lineSize)) continue; 
-					// background command	
-	 	if(!BgCmd(lineSize, jobs)) continue; 
+		if(!ExeComp(lineSize)) continue;
+					// background command
+	 	if(!BgCmd(lineSize, jobs)) continue;
 					// built in commands
 		ExeCmd(jobs, lineSize, cmdString);
-		
+
 		/* initialize for next line read*/
 		lineSize[0]='\0';
 		cmdString[0]='\0';
 	}
     return 0;
 }
+
+
+
+
 
