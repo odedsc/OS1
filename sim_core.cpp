7ab23197527bda842b7981d98b5bcac5a9c29f06
odedsc <38476537+odedsc@users.noreply.github.com>
2018-04-21 18:27:22 +0300

    Update sim_core.cpp

--- sim_core.cpp
+++ sim_core.cpp
@@ -9,6 +9,12 @@ SIM_coreState coreState;
 bool STALL; // the MIPS needs Stall
 bool READ; // true if there is an instruction which needs for MEM stage more then one clock cycle
 
+typedef struct {
+    int32_t data;  // The main data of the cmd, such as ALU result for ADD, branch condition.
+    int32_t address;  // Addresses, such as the jump address for branch, and mem address for load, store
+} StageData;
+
+StageData Stages[SIM_PIPELINE_DEPTH]; // this array is used to store the MidData of each stage in the pipeline
 		   /*implementation of the 5 stages in pipeline MIPS processor
 		   */
 
@@ -36,7 +42,89 @@ void SIM_DECODE(){
 	else{
 		(coreState.pipeStageState[1]).src2Val=coreState.regFile[((coreState.pipeStageState[1]).cmd).src2];
 }
+	
+void EmptyCertainStage (PipeStageState* stage)
+{
+	stage->src1Val = 0;
+	stage->src2Val = 0;
+	(&(stage->cmd))->opcode = 0;
+	(&(stage->cmd))->src1 = 0;
+	(&(stage->cmd))->src2 = 0;
+	(&(stage->cmd))->isSrc2Imm = 0;
+	(&(stage->cmd))->dst = 0;
+}
 
+
+void Execute() {
+	if (READ) //Stall the core if waiting for data memory reading
+	{
+		return;
+	}
+	if(STALL)
+	{
+		EmptyCertainStage(&(core->pipeStageState[2]));
+		/*
+		loadForwardStall = false;
+		*/
+	}
+	else {
+		PipeStageState ID = core->pipeStageState[1] ;
+		PipeStageState EXE = core->pipeStageState[2] ;
+		SIM_cmd IDcmd = ID.cmd ;
+		EXE = ID; //Move the state to the following stage.
+		int32_t dstVal = core->regFile[(IDcmd).dst];
+		switch (IDcmd.opcode)
+		{
+			case (CMD_NOP):
+			{
+				EmptyCertainStage(&(EXE));
+			} break;
+			case (CMD_ADD):
+			{
+				Stages[2].mainData = (ID).src1Val + (ID).src2Val;
+			} break;
+			case (CMD_SUB):
+			{
+				Stages[2].mainData = (ID).src1Val - (ID).src2Val;
+			} break;
+			case (CMD_ADDI):
+			{
+				Stages[2].mainData = (ID).src1Val + (ID).src2Val;
+			} break;
+			case (CMD_SUBI):
+			{
+				Stages[2].mainData = (ID).src1Val - (ID).src2Val;
+			} break;
+			case (CMD_LOAD):
+			{
+				Stages[2].address = (ID).src1Val + (ID).src2Val;
+			} break;
+			case (CMD_STORE):
+			{
+				Stages[2].address = dstVal + (ID).src2Val;
+			} break;
+			case (CMD_BR):
+			{
+				Stages[2].address =  Stages[1].pc + dstVal;
+			} break;
+			case (CMD_BRNEQ):
+			{
+				Stages[2].address =  Stages[1].pc + dstVal;
+			} break;
+			case (CMD_BREQ):
+			{
+				Stages[2].address =  Stages[1].pc + dstVal;
+				Stages[2].mainData = (EXE).src1Val - (EXE).src2Val;
+			} break;
+			case (CMD_HALT):{
+			} break;
+			default : {}
+		}
+		core->pipeStageState[2] = EXE ;
+	}
+}
+
+	
 /*! SIM_CoreReset: Reset the processor core simulator machine to start new simulation
 Use this API to initialize the processor core simulator's data structures.
 The simulator machine must complete this call with these requirements met:
